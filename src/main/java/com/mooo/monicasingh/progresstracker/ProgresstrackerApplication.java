bb1317d0814eb6a6c4a4102bc5468dea26b93db7
Waquar Shamsi <waquarshamsi@outlook.com>
2024-11-27 06:27:01 +0530

    initial project setup

---
+++ src/main/java/com/mooo/monicasingh/progresstracker/ProgresstrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mooo.monicasingh.progresstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgresstrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgresstrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mooo/monicasingh/progresstracker/ProgresstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooo.monicasingh.progresstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgresstrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
